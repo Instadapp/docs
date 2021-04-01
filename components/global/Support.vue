4b0bb9e439ff3909ea91b23feb869e2c072afa36
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-04-02 01:44:09 +0300

    fixes

---
+++ components/global/Support.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <hr>
+    <p>If you still can't find something you're looking for or have any questions, ask them at our developers community on <a
+        href="https://discord.gg/83vvrnY"
+        target="_blank"
+        title="Opens in a new window"
+      >Discord</a> or simply send an <a href="mailto:info@instadapp.io">Email</a>.</p>
+    <p>Our team is excited about assisting you to build an application using Instadapp Developer Platform. Your questions and feedback help us make a better development environment for future developers.</p>
+  </div>
+</template>
