4a5d761cbf5c59d0bd85c72db3210303e02e1ff4
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-03-21 14:26:46 +0200

    remove quote from desc in yaml

--- scripts/utils/generateMd.js
+++ scripts/utils/generateMd.js
@@ -1,4 +1,4 @@
-export default async function generateMd (data, address, chain) {
+export default async function generateMd(data, address, chain) {
     let nativeToken
     let explorerLink
     if (chain === 'mainnet') {
@@ -16,13 +16,13 @@ export default async function generateMd (data, address, chain) {
     } else if (chain === 'optimism') {
         nativeToken = "ETH"
         explorerLink = `[Etherscan](https://optimistic.etherscan.io/address/${address}#code)`
-    }  else {
+    } else {
         nativeToken = "ETH"
         explorerLink = `[Etherscan](https://etherscan.io/address/${address}#code)`
     }
     let md = '---'
     md += `\ntitle: ${data.title}`
-    md += `\ndescription: \'${data.description}\'`
+    md += `\ndescription: ${data.description}`
     md += '\ndraft: true'
     md += '\n---'
     md += `\n> ${data.description}`
@@ -33,38 +33,38 @@ export default async function generateMd (data, address, chain) {
     md += '\n- use -1 or `dsa.maxValue` for the maximum amount in function.'
     md += '\n- If not sure about the arguments `getId` and `setId`, pass 0.'
     for (let index = 0; index < data.functions.length; index++) {
-      const func = data.functions[index]
-      md += '\n'
-      md += '\n---'
-      md += '\n'
-      md += `\n### ${func.functionName.charAt(0).toUpperCase() + func.functionName.slice(1)}`
-      md += '\n'
-      md += `\n${func.description}`
-      md += '\n'
-      md += '\n```javascript'
-      md += '\nspells.add({'
-      md += `\n  connector: "${data.connectorId}",`
-      md += `\n  method: "${func.functionName}",`
-      md += `\n  args: [${func.parameters.map(o => o.name).join(', ')}]`
-      md += '\n});'
-      md += '\n```'
-      md += '\n'
-  
-      md += '\n<table class="table">'
-      md += '\n  <tr>'
-      md += '\n    <th>Parameter</th>'
-      md += '\n    <th>Type</th>'
-      md += '\n    <th>Description</th>'
-      md += '\n  </tr>'
-      for (let index = 0; index < func.parameters.length; index++) {
-        const parameter = func.parameters[index]
-        md += '\n   <tr>'
-        md += `\n     <td><code>${parameter.name}</code></td>`
-        md += `\n     <td>${parameter.type}</td>`
-        md += `\n     <td>${parameter.description}</td>`
-        md += '\n   <tr>'
-      }
-      md += '\n</table>'
+        const func = data.functions[index]
+        md += '\n'
+        md += '\n---'
+        md += '\n'
+        md += `\n### ${func.functionName.charAt(0).toUpperCase() + func.functionName.slice(1)}`
+        md += '\n'
+        md += `\n${func.description}`
+        md += '\n'
+        md += '\n```javascript'
+        md += '\nspells.add({'
+        md += `\n  connector: "${data.connectorId}",`
+        md += `\n  method: "${func.functionName}",`
+        md += `\n  args: [${func.parameters.map(o => o.name).join(', ')}]`
+        md += '\n});'
+        md += '\n```'
+        md += '\n'
+
+        md += '\n<table class="table">'
+        md += '\n  <tr>'
+        md += '\n    <th>Parameter</th>'
+        md += '\n    <th>Type</th>'
+        md += '\n    <th>Description</th>'
+        md += '\n  </tr>'
+        for (let index = 0; index < func.parameters.length; index++) {
+            const parameter = func.parameters[index]
+            md += '\n   <tr>'
+            md += `\n     <td><code>${parameter.name}</code></td>`
+            md += `\n     <td>${parameter.type}</td>`
+            md += `\n     <td>${parameter.description}</td>`
+            md += '\n   <tr>'
+        }
+        md += '\n</table>'
     }
     return md
-  }
+}
