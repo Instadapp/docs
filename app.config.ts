a1081f264d9ae855eadcba56699bcaac80d5bc11
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-11-24 16:53:48 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,12 @@ export default defineAppConfig({
   docus: {
     title: 'Instadapp Docs',
     description: '',
+    url: "https://docs.instadapp.io",
     image: '/preview.png',
+    socials: {
+      twitter: '@Instadapp',
+      github: 'Instadapp/dsa-connect',
+    },
   },
   aside: {
     level: 0,
