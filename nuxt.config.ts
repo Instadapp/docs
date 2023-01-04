99cf85944f31c8a916b129a431b6dd4da129824b
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2023-01-04 14:16:13 +0200

    docsearch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
     "~/assets/css/app.css",
   ],
 
+  runtimeConfig: {
+    public: {
+      algolia: {
+        apiKey: "60a01900a4b726d667eab75b6f337592",
+        applicationId: "1V8G7N9GF0",
+        docSearch: {
+          indexName: "nuxtjs",
+        },
+      }
+    }
+  },
+
   content: {
     documentDriven: true,
     highlight: {
