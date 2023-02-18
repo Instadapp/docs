b63d9013d2098d7c636fffc1a8775d1d9e78c637
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2023-02-18 18:48:31 +0200

    Update chat.post.ts

--- server/api/chat.post.ts
+++ server/api/chat.post.ts
@@ -1,23 +1,29 @@
 import { HNSWLib } from "langchain/vectorstores.js";
 import { OpenAIEmbeddings } from 'langchain/embeddings.js';
 import { makeChain } from "../util";
+import fs from 'fs';
 
 const { opeanAiKey } = useRuntimeConfig();
 
 
 let cachedChain: any;
+let directory = 'public/data'
+
+if (process.env.VERCEL || process.env.NITRO_PRESET === "vercel") {
+  directory = '/var/task/data'
+
+  console.log(
+    fs.readdirSync("/var/task")
+  )
+
+}
 
 const getChain = async () => {
   if (cachedChain) {
     return cachedChain
   }
-
-  console.log(
-    import.meta,
-    globalThis,
-    process.cwd()
-  )
-  const vectorstore = await HNSWLib.load(process.env.VERCEL || process.env.NITRO_PRESET === "vercel" ? 'data' : 'public/data', new OpenAIEmbeddings({
+  
+  const vectorstore = await HNSWLib.load(directory, new OpenAIEmbeddings({
     openAIApiKey: opeanAiKey,
   }))
 
